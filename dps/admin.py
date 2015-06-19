1b5a885a009c4edf9c8672e1d86148a47cfb747a
Greg Brown <gregplaysguitar@gmail.com>
2015-06-19 12:53:19 +1200

    deprecation fix

--- dps/admin.py
+++ dps/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 from .models import Transaction
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('amount', 'status', 'transaction_type', 'content_object', 
-                    'created',)
+    list_display = ('amount', 'status', 'transaction_type', 
+                    'content_object', 'created',)
     search_fields = ('secret', )
 
 
-class TransactionInlineAdmin(generic.GenericTabularInline):
+class TransactionInlineAdmin(GenericTabularInline):
     model = Transaction
 
     def has_add_permission(self, request):
